e3fbfd3652313e386afa481e89cac7625cd4d9a7
DataNick <DataNick@users.noreply.github.com>
2018-06-28 10:20:56 -0400

    Re-ran every file to identify bug

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,36 @@
-const path = require('path');
 
+const path = require('path'); // import Node.js path module for path related operations
+// create our config object
 const config = {
-  entry: path.join(__dirname, '/client/index.js'),
-  output: {
-    path: path.join(__dirname, './public'),
-    filename: 'bundle.js'
+  entry: path.join(__dirname, '/client/index.js'), // Abosolute path to our entry file
+  output: { // our output configuration
+    path: path.join(__dirname, './public/'), // output path (directory/folder)
+    filename: 'bundle.js' // output bundled file name
   },
-  module: {
+  module: { // define our loaders here
+    // array of rules to handle different file types
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        loader: 'babel-loader'
+        test: /\.(js|jsx)$/, // check for .js and .jsx files (uses Regex)
+        loader: 'babel-loader' // use this loader for .js and .jsx files found
       },
       {
+        // check for files ending with  .css (uses Regex)
         test: /\.css$/,
+         // use these loaders of .css files. 'css-loader gets run first and is
+         // used to handle the imports of our css files inside our jsx files.
+         // The style loader then mounts our css in to the DOM
         loaders: ['style-loader', 'css-loader']
       }
     ]
   },
-
+  // set the file extensions we want webpack to resolve
   resolve: {
     extensions: ['.js', '.jsx']
   }
 };
 
+// export our config object.
+// You may have noticed we are using es5 syntax here. This is because Webpack, which would be using this
+// file, expects es5 syntax
 module.exports = config;
